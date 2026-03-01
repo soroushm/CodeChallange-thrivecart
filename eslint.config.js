9d999a54f9f0b7ecce7c29ed798eaf4b74ec6fcc
Masoud Soroush <masoud.s.geo@gmail.com>
2026-03-01 16:52:16 +0100

    feat(lint): setup prettier & config advance eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsEslint from 'typescript-eslint'
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -10,14 +14,30 @@ export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
+      react.configs.flat.recommended,
       js.configs.recommended,
-      tseslint.configs.recommended,
+      tsEslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
-      ecmaVersion: 2020,
+      parser: tsEslint.parser,
+      ecmaVersion: 2023,
+      sourceType: 'module',
       globals: globals.browser,
     },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
+
 ])
