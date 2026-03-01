632348d399e95c2ea35eddb8a28b2eff214d5f0f
Masoud Soroush <masoud.s.geo@gmail.com>
2026-03-01 16:57:21 +0100

    chore(lint): run lint --fix

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,25 +3,25 @@ import App from './App'
 import { describe, it, expect } from 'vitest'
 
 describe('First Test', () => {
-    it('render Header correctly', () => {
-        render(<App />)
-        expect(screen.getByText('Vite + React')).toBeInTheDocument()
-    })
+  it('render Header correctly', () => {
+    render(<App />)
+    expect(screen.getByText('Vite + React')).toBeInTheDocument()
+  })
 
-    it('initial count check', () => {
-        render(<App />)
-        expect(screen.getByRole('button')).toHaveTextContent('count is 0')
-    })
+  it('initial count check', () => {
+    render(<App />)
+    expect(screen.getByRole('button')).toHaveTextContent('count is 0')
+  })
 
-    it('increments count on clicked', () => {
-        render(<App />)
+  it('increments count on clicked', () => {
+    render(<App />)
 
-        const button = screen.getByRole('button')
+    const button = screen.getByRole('button')
 
-        fireEvent.click(button)
-        expect(button).toHaveTextContent('count is 1')
+    fireEvent.click(button)
+    expect(button).toHaveTextContent('count is 1')
 
-        fireEvent.click(button)
-        expect(button).toHaveTextContent('count is 2')
-    })
+    fireEvent.click(button)
+    expect(button).toHaveTextContent('count is 2')
+  })
 })
