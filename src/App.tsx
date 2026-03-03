5e612fcbb91ea3ac3bf71070ba418b9f997504ea
Masoud Soroush <masoud.s.geo@gmail.com>
2026-03-03 14:13:03 +0100

    feat: add basic catalogue component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import './App.css'
 import { Header } from './theme/Header'
+import { Catalogue } from './components/Catalogue'
+
 function App() {
   return (
     <>
       <Header />
-      <div></div>
+      <div>
+        <Catalogue />
+      </div>
     </>
   )
 }
---
+++ src/components/Catalogue/Catalogue.tsx
@@ -0,0 +1,11 @@
+import './catalogue.css'
+export function Catalogue() {
+  return (
+    <main className="content">
+      <hgroup>
+        <h1>Catalogue</h1>
+        <p>Select widgets to add to your current sales order.</p>
+      </hgroup>
+    </main>
+  )
+}
