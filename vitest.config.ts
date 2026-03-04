6a9b39d7e059c5c7c8743eb7a2fc47af50dfe916
Masoud Soroush <masoud.s.geo@gmail.com>
2026-03-04 15:01:42 +0100

    feat(test): config test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov'],
+      reportsDirectory: './coverage',
+    },
   },
 })
